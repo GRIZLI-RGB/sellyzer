f1660db9d5d0781a3ffd3ea96e0eca115997d538
GRIZLI-RGB <ek916569@gmail.com>
2025-10-08 09:15:47 +0500

    Added blog footer

--- blog/astro.config.mjs
+++ blog/astro.config.mjs
@@ -3,10 +3,11 @@
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
 import { defineConfig } from "astro/config";
+import { SITE_URL } from "./src/consts";
 
 // https://astro.build/config
 export default defineConfig({
-	site: "https://sellyzer.ru",
+	site: SITE_URL,
 	integrations: [mdx(), sitemap()],
 	base: "/blog",
 	server: {
